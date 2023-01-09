bee63eaa7b05b005cb91be0164d535908d59e393
leafes <xmm.geek@gmail.com>
2023-01-10 04:52:32 +0800

    add search by city

--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
 import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
 
 const BOT_TOKEN = '5859830543:AAEarTywBjksPECYxhU4JsfXyGa0KaXokcM';
 const bot = new Telegraf(BOT_TOKEN);
 
-const getWeatherData = async () => {
-  const f = await fetch('https://api.open-meteo.com/v1/forecast?latitude=51.51&longitude=-0.13&current_weather=true')
+const getWeatherData = async (cityName) => {
+  const getCityGeolocation = async (cityName) => {
+    console.log('City to find: '+ cityName);
+    const f = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}`);
+    const result = await f.json();
+    const response = result.results[0] || null;
+    return response;
+  };
+  const {latitude, longitude, country} = await getCityGeolocation(cityName);
+  const f = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
   const result = await f.json();
-  console.log(result);
-  return result;
+  return {...result, country, city: cityName};
 };
 
 const formatWeatherData = (data) => {
   const {temperature, time} = data["current_weather"];
-  return `Temperature: ${temperature} Celcius,\nTime in London: ${time.slice(-5)}`
+  const {country, city} = data;
+  return `${city} / ${country} — ${temperature} Celcius\n———\nData collected at: ${time.slice(-5)} GMT`
 };
 
 bot.start((ctx) => {
-  ctx.reply('Hi! Ninusya, can you write /rap?');
+  ctx.reply('Send me the city name to get the weather infomation');
 });
-bot.command('weather', async (ctx) => {
-  const weatherData = await getWeatherData();
-  const formattedWeatherData = formatWeatherData(weatherData);
-  ctx.reply(formattedWeatherData);
-  console.log(formattedWeatherData);
-});
-bot.command('rap', (ctx) => {
-  ctx.reply('rap? ☺️');
-  console.log('Replayed to rap');
-});
+bot.on(message('text'), async (ctx) => {
+    const city = ctx.message.text;
+    ctx.reply(`City is: ${city}\nFetching info...`);
+    const weatherData = await getWeatherData(city);
+    ctx.reply(formatWeatherData(weatherData));
+  });
+
+
 bot.launch();
 console.log('rabotaem');
+
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
