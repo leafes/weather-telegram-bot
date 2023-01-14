1c236f7ee328a07c283603fe66f1ea83e1158fb8
leafes <xmm.geek@gmail.com>
2023-01-14 16:02:56 +0800

    refactor: move getWeather to another module

---
+++ src/getWeather.js
@@ -0,0 +1,19 @@
+const getWeatherData = async (city) => {
+  const getCityGeolocation = async (city) => {
+    console.log('City to find: '+ city);
+    const f = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`);
+    const result = await f.json();
+    if (Object.keys(result).length < 2) return null;
+    const response = result.results[0];
+    return response;
+  };
+  const geolocation = await getCityGeolocation(city);
+  if (geolocation === null) return null;
+
+  const {latitude, longitude, country} = geolocation;
+  const f = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
+  const result = await f.json();
+  return {...result, country, city};
+};
+
+export default getWeatherData;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
+import getWeatherData from "./getWeather.js";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
-// TODO: Move API requests to another module 
-const getWeatherData = async (city) => {
-  const getCityGeolocation = async (city) => {
-    console.log('City to find: '+ city);
-    const f = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`);
-    const result = await f.json();
-    if (Object.keys(result).length < 2) return null;
-    const response = result.results[0];
-    return response;
-  };
-  const geolocation = await getCityGeolocation(city);
-  if (geolocation === null) return null;
-
-  const {latitude, longitude, country} = geolocation;
-  const f = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
-  const result = await f.json();
-  return {...result, country, city};
-};
 
 // Move formatters to another module
 const formatWeatherData = (data) => {
