540c888e1a329084a0b4da63b7708056e2bee136
leafes <xmm.geek@gmail.com>
2023-01-14 19:58:56 +0800

    minor fixes

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,14 +1,17 @@
+// add try 
+
+const getCityGeolocation = async (city) => {
+  console.log('City to find: '+ city);
+  const f = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`);
+  const result = await f.json();
+  if (Object.keys(result).length < 2) return { err: 'City not found' };
+  const response = result.results[0];
+  return response;
+};
+
 const getWeatherData = async (city) => {
-  const getCityGeolocation = async (city) => {
-    console.log('City to find: '+ city);
-    const f = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`);
-    const result = await f.json();
-    if (Object.keys(result).length < 2) return null;
-    const response = result.results[0];
-    return response;
-  };
   const geolocation = await getCityGeolocation(city);
-  if (geolocation === null) return null;
+  if (geolocation.err) return null;
 
   const {latitude, longitude, country, name} = geolocation;
   const f = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
